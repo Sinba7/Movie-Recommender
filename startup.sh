156d508c3c7d850f8a3f54e9942003408542db19
Sinba7 <zhangny3@gmail.com>
2020-12-15 22:11:31 -0800

    update startup.sh/README

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 echo 'istart nstalling required packages'
 pip install -r requirements.txt
 echo 'successfully installed required packages'
-echo 'start the movie recommendation app'
-python src/dash_app.py
+echo 'starting the movie recommendation app'
+gunicorn src.dash_app:app_server
