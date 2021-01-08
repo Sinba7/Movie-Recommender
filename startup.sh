e4f0121e17e9369685f10e43179d03f929efe962
Sinba7 <zhangny3@gmail.com>
2021-01-08 15:19:01 -0800

    update comments

--- startup.sh
+++ startup.sh
@@ -2,9 +2,9 @@ echo 'Start installing required packages'
 pip install -r requirements.txt
 echo 'Successfully installed required packages'
 
-echo 'Test functions'
+echo 'Start testing'
 pytest --cov-report term-missing --cov='src'
 echo 'Pass all tests'
 
-echo 'Starting the movie recommendation app'
+echo 'Initializing the movie recommendation app'
 gunicorn src.dash_app:app_server
